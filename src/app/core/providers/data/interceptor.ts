7c1ff5d7f0150cb98eb7f3d01761d2f177be3ccb
psychomet <sanatkar.dev@gmail.com>
2023-02-27 00:22:01 +0330

    ``    wip``

--- src/app/core/providers/data/interceptor.ts
+++ src/app/core/providers/data/interceptor.ts
@@ -34,7 +34,7 @@ export class DefaultInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req.clone({
             params: (req.params ? req.params : new HttpParams())
-                .set('languageCode', 'fa')
+                .set('languageCode', 'fa').set('vendure-token','gelfc0hgublfjkpry3fg')
         })).pipe(
             tap(
                 event => {
